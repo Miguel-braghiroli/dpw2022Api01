f002d4ec03e0ed507b8261b62cdab1d12f1b7317
Miguel-braghiroli <miguelangelbraghirolilemus@gmail.com>
2022-02-26 01:36:39 -0600

    CRUD de equipos completado

--- equiposWebApi/Controllers/equiposController.cs
+++ equiposWebApi/Controllers/equiposController.cs
@@ -28,13 +28,70 @@ namespace equiposWebApi.Controllers
         [Route("api/equipos")]
         public IActionResult Get()
         {
-            IEnumerable<equipos> equiposList = from e in _context.equipos
-                                               select e;
+            IEnumerable<equipos> equiposList = (from e in _context.equipos
+                                               select e);
             if (equiposList.Count()>0)
             {
                 return Ok(equiposList);
             }
             return NotFound();
+        }
+
+        [HttpGet]
+        [Route("api/equipos/{idUsuarios}")]
+        public IActionResult Get(int idUsuario)
+        {
+            equipos equipo = (from e in _context.equipos
+                                                where e.id_equipos == idUsuario
+                                                select e
+                                                ).FirstOrDefault();
+
+            if (equipo != null)
+            {
+                return Ok(equipo);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        [Route("api/equipos")]
+        public IActionResult guardarEquipo([FromBody] equipos equipoNuevo)
+        {
+            try
+            {
+                _context.equipos.Add(equipoNuevo);
+                _context.SaveChanges();
+                return Ok(equipoNuevo);
+            }
+            catch (System.Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+       [HttpPut]
+       [Route("api/equipos")]
+       public IActionResult updateEquipo([FromBody] equipos equipoAModificar)
+        {
+            //Para actualizar un registro, se obtiene el registro original de la base de datos
+            equipos equipoExiste = (from e in _context.equipos
+                                    where e.id_equipos == equipoAModificar.id_equipos
+                                    select e).FirstOrDefault();
+            if (equipoExiste is null)
+            {
+                // Si no existe el registro retornar un NO ENCONTRADO
+                return NotFound();
+            }
+
+            //Si se encuentra el registro, se alteran los campos a modificar
+            equipoExiste.nombre = equipoAModificar.nombre;
+            equipoExiste.descripcion = equipoAModificar.descripcion;
+
+            //Se envia el objeto a la base de datos.
+            _context.Entry(equipoExiste).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok(equipoExiste);
         }
     }
 }
