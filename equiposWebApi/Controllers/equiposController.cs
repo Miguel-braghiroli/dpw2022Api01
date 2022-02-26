00b212a1e8e4914300b738b1c3dd4a7850d5681d
Miguel-braghiroli <miguelangelbraghirolilemus@gmail.com>
2022-02-25 23:37:44 -0600

    first commit

---
+++ equiposWebApi/Controllers/equiposController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using equiposWebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace equiposWebApi.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class equiposController : ControllerBase
+    {
+        private readonly prestamosContext _context;
+
+        public equiposController(prestamosContext miContexto)
+        {
+            this._context = miContexto;
+        }
+
+        ///<summary>
+        ///Metodo para retornar todos los reg. de la tabla de EQUIPOS
+        ///</summary>
+        ///<returns></returns>
+        [HttpGet]
+        [Route("api/equipos")]
+        public IActionResult Get()
+        {
+            IEnumerable<equipos> equiposList = from e in _context.equipos
+                                               select e;
+            if (equiposList.Count()>0)
+            {
+                return Ok(equiposList);
+            }
+            return NotFound();
+        }
+    }
+}
